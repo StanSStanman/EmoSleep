abc15404fd5ee2fde68aa98dfac7e3eae39e04d3
stansstanman <ruggero.basanisi@gmail.com>
2023-06-29 17:09:59 +0200

    initial commit, added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import os
+
+__version__ = '0.0.1'
+NAME = 'emosleep'
+AUTHOR = 'Ruggero Basanisi'
+MAINTEINER = 'Ruggero Basanisi'
+
+setup(
+    name=NAME,
+    version=__version__,
+    packages=find_packages(),
+    author=AUTHOR,
+    maintainer=MAINTEINER,
+    description="Pipeline for EmoSleep project's data analysis",
+    license='None',
+    install_requires=['numpy',
+                      'scipy',
+                      'mne',
+                      'xarray',
+                      'matplotlib']
+)
