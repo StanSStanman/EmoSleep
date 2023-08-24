0a63b8bc3293292ba48d758f237d4a84f0a92f01
stansstanman <ruggero.basanisi@gmail.com>
2023-08-24 15:10:34 +0200

    updated data path

--- emosleep/main.py
+++ emosleep/main.py
@@ -5,7 +5,7 @@ from emosleep.events import create_events
 from emosleep.epochs import create_epochs, create_baseline_epochs
 
 
-datapath = '/Disk2/EmoSleep/derivativesNEW3/'
+datapath = '/Disk2/EmoSleep/derivatives/'
 subjects = ['sub-01', 'sub-02', 'sub-03', 'sub-04', 'sub-05', 
             'sub-06', 'sub-07', 'sub-10', 'sub-11', 'sub-12', 
             'sub-14', 'sub-15', 'sub-16', 'sub-17', 'sub-19', 
@@ -21,34 +21,35 @@ bln_filename = '{0}_ses-{1}_bln-epo.fif'
 
 
 for subj in subjects:
-   # define folder names and create them
-    mne_dir = op.join(datapath,subj,'mne')
+    # define folder names and create them
+    mne_dir = op.join(datapath, subj, 'mne')
     if not op.exists(mne_dir):
         os.mkdir(mne_dir)
-    mfname = op.join(datapath,subj,mat_fname.format(subj,ses)) 
+    mfname = op.join(datapath, subj, mat_fname.format(subj, ses))
     
-    mont_dir = op.join(mne_dir,'mont')
+    mont_dir = op.join(mne_dir, 'mont')
     if not op.exists(mont_dir):
         os.mkdir(mont_dir)
-    mont_fname = op.join(mont_dir,montage_filename.format(subj,ses))
+    mont_fname = op.join(mont_dir, montage_filename.format(subj, ses))
 
-    digimontage = compute_montage(mfname,mont_fname)
+    digimontage = compute_montage(mfname, mont_fname)
 
-    eve_dir = op.join(mne_dir,'eve')
+    eve_dir = op.join(mne_dir, 'eve')
     if not op.exists(eve_dir):
         os.mkdir(eve_dir)
-    eve_fname = op.join(eve_dir,events_filename.format(subj,ses))
+    eve_fname = op.join(eve_dir, events_filename.format(subj, ses))
 
-    events = create_events(mfname,eve_fname)
+    events = create_events(mfname, eve_fname)
 
-    epo_dir = op.join(mne_dir,'epo')
+    epo_dir = op.join(mne_dir, 'epo')
     if not op.exists(epo_dir):
         os.mkdir(epo_dir)
-    epo_fname = op.join(epo_dir,epo_filename.format(subj,ses))
+    epo_fname = op.join(epo_dir, epo_filename.format(subj, ses))
     ev_id = {'reject': 0, 'negative': 1, 'neutral': 2, 'positive': 3}
-    epochs = create_epochs(mfname,mont_fname,eve_fname,epo_fname,ev_id=ev_id)
+    epochs = create_epochs(mfname, mont_fname, eve_fname, epo_fname,
+                           ev_id=ev_id)
 
-    
-    bln_fname = op.join(epo_dir,bln_filename.format(subj,ses))
+    bln_fname = op.join(epo_dir, bln_filename.format(subj, ses))
 
-    baseline = create_baseline_epochs(mfname,mont_fname,eve_fname,bln_fname,ev_id=ev_id)
+    baseline = create_baseline_epochs(mfname, mont_fname, eve_fname,
+                                      bln_fname, ev_id=ev_id)
--- emosleep/montage.py
+++ emosleep/montage.py
@@ -29,13 +29,8 @@ def compute_montage(mat_fname, mont_fname=False):
             dig_mont.save(mont_fname, overwrite=True)
         else:
             raise ValueError('mont_fname should be False or path-like object')
-<<<<<<< HEAD
    # dig_mont.plot(show=False, kind='3d')
    # plt.show(block=True)
-=======
-    dig_mont.plot(show=False)
-    plt.show(block=True)
->>>>>>> af32e7186856408000eb0d04c6c6d5b306f1c480
     return dig_mont
 
 
