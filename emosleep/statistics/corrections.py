d3245f7a2d68e8303ba5496c7f85007d4e131260
stansstanman <ruggero.basanisi@gmail.com>
2023-09-04 14:07:23 +0200

    added correction method fdr

---
+++ emosleep/statistics/corrections.py
@@ -0,0 +1,21 @@
+import xarray as xr
+from statsmodels.stats.multitest import fdrcorrection
+
+
+def fdr_correction(summary):
+    pvals = summary.pvalue.values.flatten()
+    rej_pv, corr_pv = fdrcorrection(pvals=pvals,
+                                    alpha=.05,
+                                    method='indep',
+                                    is_sorted=False)
+    
+    rej_pv = rej_pv.reshape(summary.pvalue.shape)
+    
+    corr_pv = xr.DataArray(corr_pv.reshape(summary.pvalue.shape),
+                           coords=summary.pvalue.coords,
+                           dims=summary.pvalue.dims,
+                           name='corr_pval')
+    
+    summary = xr.merge([summary, corr_pv])
+    
+    return summary, rej_pv
